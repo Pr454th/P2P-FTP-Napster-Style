531251fc3b8af1e68e09ffe995909e4140942459
Pradesh <pradeshgv@gmail.com>
2022-12-25 22:55:34 +0530

    Main Menu Modified and MyMain.class file added

--- MyMain.java
+++ MyMain.java
@@ -2,23 +2,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class MyMain {
-	public static void main(String args[]) throws Exception{
-
-		System.out.println("Please enter your Choice ::");
+	public static void main(String args[]) throws Exception {
+		System.out.println("*** P2P FTP using Centralized Directory Architecture ***");
 		System.out.println("1. To run the Server");
 		System.out.println("2. To run the Client");
-		
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		System.out.print("Please enter your Choice : ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int choice = Integer.parseInt(br.readLine());
-		
-		if(choice == 1){		
+
+		if (choice == 1) {
 			Server s = new Server();
-		}
-		else if(choice == 2){
+		} else if (choice == 2) {
 			Client c = new Client();
-		}
-		else{
+		} else {
 			System.out.println("Your choice is incorrect");
 		}
 	}
